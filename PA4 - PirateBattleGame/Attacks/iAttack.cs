93cac05e64188bc4d44dd121a62dd2c89baac70b
Ethan Dykes <dykes.e.g@gmail.com>
2025-04-02 22:50:37 -0500

    Delete PA4 - PirateBattleGame directory
    2.0 Version Being Uploaded

--- PA4 - PirateBattleGame/Attacks/iAttack.cs
+++
@@ -1,8 +0,0 @@
-namespace PirateBattleGame.Attacks
-{
-    public interface IAttack
-    {
-        (int damage, bool isCrit, bool isMiss) ExecuteAttack(int basePower);
-        string GetAttackName();
-    }
-}
